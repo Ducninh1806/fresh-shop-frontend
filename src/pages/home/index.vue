e97c879a7b6510652b513a8fbeda7b1e61ee6eaa
Nguyễn Đức Ninh - CMC Global DU3 <ndninh@cmcglobal.vn>
2021-10-20 17:07:27 +0700

    add template

---
+++ src/pages/home/index.vue
@@ -0,0 +1,24 @@
+<template >
+    <div>
+        <Header />
+        <LayoutHome />
+        <Footer />
+    </div>
+</template>
+<script>
+import Header from '../../components/Header.vue'
+import Footer from '../../components/Footer.vue'
+import LayoutHome from '../../layout/home/index.vue'
+export default {
+  components: { Header, LayoutHome, Footer },
+    name: 'PageHome',
+    data(){
+        return {
+
+        }
+    },
+}
+</script>
+<style >
+    
+</style>
